4c8be32347ceb247129b7b2c6f8c13033240f8cc
AKKSHAAT <akkshaat@gmail.com>
2024-12-16 15:38:13 +0530

    something

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,6 +4,7 @@ const Loading: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="w-16 h-16 border-4 border-pink-500 border-solid rounded-full animate-spin border-t-transparent"></div>
+      <p>the backend is hosted on Render.com free tier this might take some time</p>
     </div>
   );
 };
